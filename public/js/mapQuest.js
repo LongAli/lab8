3a62ff7d24fc11c3cc681f5cd731fc6c67cbda89
Ali L <alil@alis-mbp-6.lan>
2021-02-25 10:33:43 -0800

    API Lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = '8c0493GlGjqi6RFdZo0aQYl3KlpaqMFM';
+
+// 'map' refers to a <div> element with the ID map
+
+var map = L.mapquest.map('map', {
+  center: [32.88,-117.236],
+  layers: L.mapquest.tileLayer('map'),
+  zoom: 12,
+  zoomControl: false
+});
+	//add a marker to map
+  L.marker([32.88,-117.236]).addTo(map);
 }
